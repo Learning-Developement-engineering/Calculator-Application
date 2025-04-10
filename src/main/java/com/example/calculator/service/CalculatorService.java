449c52c40ba2ad8aee7493fba5285e9619212732
Gautam Makwana <gautam.makwana@cmss.in>
2025-04-10 11:43:01 +0530

    percentage functionality added

--- src/main/java/com/example/calculator/service/CalculatorService.java
+++ src/main/java/com/example/calculator/service/CalculatorService.java
@@ -6,4 +6,7 @@ public interface CalculatorService {
     double multiply(double a, double b);
     double divide(double a, double b);
     double power(double base, double exponent);
+    double percentage(double value, double percent);
+    double percentageOf(double part, double total);
+    double percentageChange(double oldValue, double newValue);
 }
